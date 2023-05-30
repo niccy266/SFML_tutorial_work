d69fe5828584fadcf278ea622b4102b84eee41cf
Nicolás Concha Vargas <niccy266@gmail.com>
2023-05-29 23:09:29 -0400

    Added code to print VideoModes
    using code from https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1VideoMode.php

--- window/main.cpp
+++ window/main.cpp
@@ -9,6 +9,19 @@
 int main(int, char**) {
     sf::Window window(sf::VideoMode(800, 600), "Hello World");
 
+    // Display the list of all the video modes available for fullscreen
+    std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+    for (std::size_t i = 0; i < modes.size(); ++i)
+    {
+        sf::VideoMode mode = modes[i];
+        std::cout << "Mode #" << i << ": "
+                << mode.width << "x" << mode.height << " - "
+                << mode.bitsPerPixel << " bpp" << std::endl;
+    }
+    // Create a window with the same pixel depth as the desktop
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window");
+
     // wait for something to happen before closing window
 
     return 0;
