81c4b8409c98012309d712092dbaa73779066574
Nicolás Concha Vargas <niccy266@gmail.com>
2023-05-29 23:05:36 -0400

    Window command

--- window/main.cpp
+++ window/main.cpp
@@ -1,6 +1,15 @@
+/** SFML window tutorial
+ *  Following tutorial at https://www.sfml-dev.org/tutorials/2.5/window-window.php
+ *  Nicolas Concha May 2023
+ */
+
 #include <iostream>
 #include <SFML/Window.hpp>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    sf::Window window(sf::VideoMode(800, 600), "Hello World");
+
+    // wait for something to happen before closing window
+
+    return 0;
 }
